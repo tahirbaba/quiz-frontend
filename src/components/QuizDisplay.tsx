7feba7f1879427a5f72d54731564eb5760072880
tahirbaba <muhammadtahirhasni@gmail.com>
2025-06-22 23:17:57 +0500

    Refactored API URL to use env variable

--- src/components/QuizDisplay.tsx
+++ src/components/QuizDisplay.tsx
@@ -42,7 +42,7 @@ export default function QuizDisplay({ prompt, onFinish }: QuizDisplayProps) {
   useEffect(() => {
     const fetchQuiz = async () => {
       try {
-        const res = await fetch("http://localhost:8000/generate-quiz", {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/generate-quiz`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
